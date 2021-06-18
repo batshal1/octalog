aeae032c81b55c9a9984438819524c3fc9257a7f
Bhattarai,batshal B <b.bhattarai@student.fontys.nl>
2021-06-18 20:18:15 +0200

    Update PostService.java

--- src/main/java/com/sept/rest/webservices/restfulwebservices/post/PostService.java
+++ src/main/java/com/sept/rest/webservices/restfulwebservices/post/PostService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostService {
@@ -33,7 +34,7 @@ public class PostService {
     }
 
     public Post retrieveById(Long id){
-        return postJpaRepository.findById(id).get();
+        return postJpaRepository.findById(id).orElse(null);
     }
 
     public void DeleteById(Long id){
